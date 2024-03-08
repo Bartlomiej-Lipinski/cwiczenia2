189b9592a7b4d6154283d5c9826556cd8c90db7e
bartlomiej_lipinski <rochmisz1@gmail.com>
2024-03-08 14:58:13 +0100

    zmiana major

--- NewFile1.cs
+++ NewFile1.cs
@@ -1,1 +1,11 @@
-Co
+Console.WriteLine("Hello World!");
+Console.WriteLine(GetAvg([1,2,3]))
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+    foreach (var VARIABLE in arr)
+    {
+        sum += VARIABLE;
+    }
+    return (double)sum / arr.Length;
+}
